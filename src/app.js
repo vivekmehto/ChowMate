dd3a851b2e027153a15823c009c6d1eba9368ad1
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-11 10:43:15 +0530

    Initial commit: Set up project structure and add basic files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Body from "./components/Body";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
