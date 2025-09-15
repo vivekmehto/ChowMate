baaaa56c393b5ea1f60f09d8c8c25667202dade3
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-15 13:01:58 +0530

    UPDATE: body and Restaurant Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-  return <div>Body</div>;
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6387187&lng=77.0879235&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    console.log(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  return (
+    <div className="body">
+      {listOfRestaurants.map((res) => (
+        <RestaurantCard key={res.info.id} {...res} />
+      ))}
+    </div>
+  );
 };
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,30 +1,25 @@
 import { CDN_URL } from "../utils/constants";
 
-const RestaurantCard = (props) => {
-  const { resData } = props;
-
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = resData;
+const RestaurantCard = ({ info }) => {
+  console.log("here is res data " + info);
+  const { name, avgRating, cuisines, costForTwo, sla, cloudinaryImageId } =
+    info;
 
   return (
-    <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
+    <div className="restaurant-card">
       <img
-        className="rounded-lg"
+        className="res-img"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
       />
-      <h3 className="font-bold py-4 text-lg">{name}</h3>
+      <h3 className="res-name">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
-      <h4>User : {loggedInUser} </h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString} </h4>
+      {/* <h4>User : {loggedInUser} </h4> */}
     </div>
   );
 };
+
+export default RestaurantCard;
