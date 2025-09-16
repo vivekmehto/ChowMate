6f6c72a14e223bd6548809236b71b72851682417
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-16 13:41:30 +0530

    ADD: react-router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import About from "./components/About";
+import Error from "./components/Error";
 import Body from "./components/Body";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Contact from "./components/Contact";
 
 const AppLayout = () => {
   return (
@@ -15,6 +19,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
