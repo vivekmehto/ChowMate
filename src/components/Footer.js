da2f7d46eac7ebf71c260d36c250598352bfcc52
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-17 19:33:26 +0530

    ADD: Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,33 @@
-import React from "react";
-
+import { Link } from "react-router";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Brand / Logo */}
+        <div className="footer-logo">
+          <h2>ChowMate 🍲♟️</h2>
+          <p>Your smart food delivery partner</p>
+        </div>
+
+        {/* Links */}
+        <div className="footer-links">
+          <Link href="/about">About Us</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+
+        {/* Social Media */}
+        <div className="footer-social">
+          <Link href="https://facebook.com">Facebook</Link>
+          <Link href="https://instagram.com">Instagram</Link>
+          <Link href="https://twitter.com">Twitter</Link>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>© {new Date().getFullYear()} ChowMate. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
