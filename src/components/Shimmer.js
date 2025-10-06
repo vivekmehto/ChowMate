4da88090388edbb60d778b8a819879ea44995931
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-10-06 22:04:31 +0530

    ADD: Shimmer tailwindcss

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,44 +1,20 @@
 import React from "react";
 
 const Shimmer = () => {
+  // Array to render multiple shimmer cards
+  const shimmerArray = Array(9).fill(0);
+
   return (
-    <div className="shimmer-body">
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
-      <div className="shimmer-restaurant-card">
-        <div className="shimmer-res-img"></div>
-        <div className="shimmer-res-name"></div>
-      </div>
+    <div className="flex flex-wrap justify-center gap-4">
+      {shimmerArray.map((_, index) => (
+        <div
+          key={index}
+          className="w-64 h-72 bg-gray-200 rounded-xl overflow-hidden animate-pulse m-4 flex flex-col"
+        >
+          <div className="w-full h-40 bg-gray-300"></div>
+          <div className="h-6 bg-gray-300 m-4 rounded"></div>
+        </div>
+      ))}
     </div>
   );
 };
