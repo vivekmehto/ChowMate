1dab03ce8bbbe272071f270080f1cf2faf494a74
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-10-14 13:25:50 +0530

    ADD: mock data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,8 @@ export const CDN_URL =
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6387187&lng=77.0879235&restaurantId=";
 
+export const MENU_API_2 =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6387187&lng=77.0879235&restaurantId=331201&catalog_qa=undefined&submitAction=ENTER%20Request%20Method%20GET";
+
 export const RES_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6387187&lng=77.0879235&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
