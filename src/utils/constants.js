49bfb3bf90ed817f83371de51ea5883685b2acb2
vivekmehto <vivekmehto.webdev@gmail.com>
2025-12-01 10:14:43 +0530

    restaurant api fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API ="https://namastedev.com/api/v1/listRestaurantMenu/"
+export const MENU_API ="https://corsproxy.io/https://namastedev.com/api/v1/listRestaurantMenu/"
 
-export const RES_API = "https://namastedev.com/api/v1/listRestaurants";
+export const RES_API = "https://corsproxy.io/https://namastedev.com/api/v1/listRestaurants";
